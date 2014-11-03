9e31788fb3bdaa3c0e0b3ad7bcb2b58a2afca2fc
Jasneet Sabharwal <jasneet.sabharwal@gmail.com>
2014-11-03 00:46:16 -0800

    Initial code for Executor which is the main class for the project

---
+++ src/main/java/cmpt741/project/impl/Executor.java
@@ -0,0 +1,35 @@
+package cmpt741.project.impl;
+
+import cmpt741.project.common.HadoopConf;
+import cmpt741.project.common.Params;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+/**
+ * @author Jasneet Sabharwal (jasneet.sabharwal@sfu.com)
+ */
+public class Executor {
+
+    public static void main(String args[]) throws Exception {
+        //Create job conf for Pass1
+        Job pass1Job = HadoopConf.generateConf(MapRedSONPass1.class, MapRedSONPass1.Pass1Map.class,
+                MapRedSONPass1.Pass1Reduce.class, "jsabharw-MapRedSONPass1",
+                Text.class, IntWritable.class);
+        pass1Job.setNumReduceTasks(10);
+        pass1Job.getConfiguration().setInt(Params.MINIMUM_SUPPORT.toString(), 100);
+
+        System.out.println("INPUT PATH - " + args[0]);
+        System.out.println("OUTPUT PATH - " + args[1]);
+
+        System.out.println("############# Executing Pass1 Map Reduce #############");
+        FileInputFormat.setInputPaths(pass1Job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(pass1Job, new Path(args[1]));
+
+        System.exit(pass1Job.waitForCompletion(true) ? 0 : 1);
+    }
+}
