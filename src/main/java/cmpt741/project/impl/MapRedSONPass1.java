a745c117ad8deb6e8ee8e8b5ed40148594fa5db3
Jasneet Sabharwal <jasneet.sabharwal@gmail.com>
2014-11-03 04:15:40 -0800

    Added setup method in which reading input split and creating list of transactions

--- src/main/java/cmpt741/project/impl/MapRedSONPass1.java
+++ src/main/java/cmpt741/project/impl/MapRedSONPass1.java
@@ -1,15 +1,20 @@
 package cmpt741.project.impl;
 
+import cmpt741.project.common.Utils;
 import cmpt741.project.models.Transaction;
+import static cmpt741.project.common.Params.*;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.*;
-
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author Jasneet Sabharwal (jasneet.sabharwal@sfu.com)
@@ -19,15 +24,33 @@ public class MapRedSONPass1 {
     public static class Pass1Map extends Mapper<LongWritable, Text,
             Text, IntWritable> {
 
+        @Override
+        public void setup(Context context) throws IOException, InterruptedException {
+            String splitRegex = context.getConfiguration().get(ITEM_SPLIT.toString());
+            if (splitRegex == null) {
+                splitRegex = "\\s+";
+                System.err.println("Split regex not found");
+            }
+
+            Path path = ((FileSplit) context.getInputSplit()).getPath();
+            FileSystem fileSystem = FileSystem.get(context.getConfiguration());
+            System.out.println("Filepath: " + path.toString());
+            if (fileSystem == null) {
+                System.out.println("Filesystem is null");
+            }
+
+            List<Transaction> transactions = Utils.readTransactionsFromHadoop(path, splitRegex, fileSystem);
+
+            System.out.println("Number of transactions: " + String.valueOf(transactions.size()));
+        }
+
         public void map(LongWritable key, Text value,
                         Context context) throws IOException, InterruptedException {
             Configuration conf = context.getConfiguration();
             int numMappers = Integer.parseInt(conf.get("mapred.map.tasks"));
-            Transaction transaction =
-            Text word = new Text();
-            word.set("map1");
 
-            context.write(word, new IntWritable(1));
+
+            context.write(value, new IntWritable(1));
         }
     }
 
