8b61d49a0165ba60c9d647bdc945c081511a86e2
Jasneet Sabharwal <jasneet.sabharwal@gmail.com>
2014-11-03 00:45:10 -0800

    Interface for frequent item sets calculator.

---
+++ src/main/java/cmpt741/project/inf/IFreqItemsetCalc.java
@@ -0,0 +1,10 @@
+package cmpt741.project.inf;
+
+/**
+ * @author Jasneet Sabharwal (jasneet.sabharwal@sfu.com)
+ */
+public interface IFreqItemsetCalc {
+
+    public void getFrequentItemsets();
+
+}
