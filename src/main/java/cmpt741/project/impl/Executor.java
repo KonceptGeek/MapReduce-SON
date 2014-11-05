c9452109a93b5c6785e4008b855f18792bafa0a0
Jasneet Sabharwal <jasneet.sabharwal@gmail.com>
2014-11-04 19:49:46 -0800

    Updated executor

--- src/main/java/cmpt741/project/impl/Executor.java
+++ src/main/java/cmpt741/project/impl/Executor.java
@@ -8,6 +8,7 @@ import cmpt741.project.common.Utils;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -20,23 +21,32 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class Executor {
 
     public static void main(String args[]) throws Exception {
+        int minSupport = Integer.parseInt(args[0]);
+        String inputPath = args[1];
+        String outputPath = args[2];
+
         //Create job conf for Pass1
-        Job pass1Job = HadoopConf.generateConf(MapRedSONPass1.class, MapRedSONPass1.Pass1Map.class,
-                MapRedSONPass1.Pass1Reduce.class, "jsabharw-MapRedSONPass1",
-                Text.class, IntWritable.class, CustomFileInputFormat.class);
+        Job pass1Job = HadoopConf.generateConf(MapRedSONPass1.class,
+                MapRedSONPass1.Pass1Map.class, MapRedSONPass1.Pass1Reduce.class,
+                "jsabharw-MapRedSONPass1", Text.class, IntWritable.class,
+                Text.class, NullWritable.class, CustomFileInputFormat.class);
+
+
         //pass1Job.setNumReduceTasks(10);
-        pass1Job.getConfiguration().setInt(MINIMUM_SUPPORT.toString(), 10);
+        pass1Job.getConfiguration().setInt(MINIMUM_SUPPORT.toString(), minSupport);
         pass1Job.getConfiguration().set(ITEM_SPLIT.toString(), "\\s+");
 
-        int totalLineCount = Utils.getLinesInHadoopFile(new Path(args[0]), FileSystem.get(pass1Job.getConfiguration()));
-        pass1Job.getConfiguration().setInt(TOTAL_TRANSACTIONS.toString(), totalLineCount);
+        //int totalLineCount = Utils.getLinesInHadoopFile(new Path(inputPath), FileSystem.get(pass1Job.getConfiguration()));
+        pass1Job.getConfiguration().setInt(TOTAL_TRANSACTIONS.toString(), 100000);
 
-        System.out.println("INPUT PATH - " + args[0]);
-        System.out.println("OUTPUT PATH - " + args[1]);
 
+        System.out.println("INPUT PATH - " + inputPath);
+        System.out.println("OUTPUT PATH - " + outputPath);
+        
         System.out.println("############# Executing Pass1 Map Reduce #############");
-        FileInputFormat.setInputPaths(pass1Job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(pass1Job, new Path(args[1]));
+
+        FileInputFormat.setInputPaths(pass1Job, new Path(inputPath));
+        FileOutputFormat.setOutputPath(pass1Job, new Path(outputPath));
 
         System.exit(pass1Job.waitForCompletion(true) ? 0 : 1);
     }
